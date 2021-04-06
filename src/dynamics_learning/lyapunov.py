04680d682296d9550c585654aae28e2103d4d1da
Bassel Mabsout <bmabsout@hotmail.com>
2021-04-06 16:08:12 -0400

    updated to learn a control lyapunov function as well as a controller

--- src/dynamics_learning/lyapunov.py
+++ src/dynamics_learning/lyapunov.py
@@ -10,74 +10,89 @@ from tensorflow.keras import layers
 from tensorflow.python.keras import losses
 import argparse
 
-def compile_lyapunov_model(state_shape):
+def V_def(state_shape):
+	inputs = keras.Input(shape=state_shape)
+	dense1 = layers.Dense(1023, activation='relu')(inputs)
+	dense2 = layers.Dense(1023, activation='relu')(dense1)
+	outputs = layers.Dense(1)(dense2)
+	model = keras.Model(inputs=inputs, outputs=outputs, name="V")
+	print()
+	print()
+	print("V:")
+	model.summary()
+	return model
 
-	def V_def():
-		inputs = keras.Input(shape=state_shape)
-		dense1 = layers.Dense(64, activation='relu')(inputs)
-		dense2 = layers.Dense(64, activation='relu')(dense1)
-		outputs = layers.Dense(1)(dense2)
-		model = keras.Model(inputs=inputs, outputs=outputs, name="V")
-		print()
-		print()
-		print("V:")
-		model.summary()
-		return model
+def actor_def(state_shape, action_shape):
+	inputs = keras.Input(shape=state_shape)
+	dense1 = layers.Dense(64)(inputs)
+	outputs = layers.Dense(np.squeeze(action_shape))(dense1)
+	model = keras.Model(inputs=inputs, outputs=outputs)
+	model.summary()
+	return model
 
-	def full_model_def(V):
-		input_x=keras.Input(shape=state_shape)
-		input_fxu=keras.Input(shape=state_shape)
-		input_0=keras.Input(shape=state_shape)
+def full_model_def(f, actor, V, state_shape):
+	input_x=keras.Input(shape=state_shape)
+	# input_fxu=keras.Input(shape=state_shape)
+	action = actor(input_x)
+	fxu = f([input_x, action])
+	input_0=keras.Input(shape=state_shape)
 
-		model = keras.Model(inputs=[input_x,input_fxu,input_0], outputs=[V(input_x),V(input_fxu),V(input_0)], name="full_model")
-		print()
-		print()
-		print("full_model:")
-		model.summary()
-		return model
-
-
-	def lyapunov_loss(_, y_pred):
-		# V((1,0,0)) = 0 for no movement and pointing upwards
-		# V(f(x,u)) - V(x) < 0
-                # V(x) > 0
-		V_x = y_pred[0]
-		V_fxu = y_pred[1]
-		V_0 = y_pred[2]
-		return tf.reduce_mean(V_0**2) + args.hyper_diff*tf.reduce_mean(tf.nn.relu(args.epsilon + V_x - V_fxu)) + args.hyper_psd * tf.reduce_mean(tf.nn.relu(args.epsilon - V_x))
+	model = keras.Model(inputs=[input_x,input_0], outputs=[V(input_x),V(fxu),V(input_0)], name="full_model")
+	print()
+	print()
+	print("full_model:")
+	model.summary()
+	return model
 
 
-	full_model = full_model_def(V_def())
-	full_model.compile(loss=lyapunov_loss, optimizer=keras.optimizers.Adam(lr=args.lr))
-	return full_model
+def lyapunov_loss(_, y_pred):
+	# V((1,0,0)) = 0 for no movement and pointing upwards
+	# V(f(x,u)) - V(x) < 0
+            # V(x) > 0
+	V_x = y_pred[0]
+	V_fxu = y_pred[1]
+	V_0 = y_pred[2]
+	return tf.reduce_mean(V_0**2) + args.hyper_diff*tf.reduce_mean(tf.nn.relu(args.epsilon + V_fxu - V_x )) + args.hyper_psd * tf.reduce_mean(tf.nn.relu(args.epsilon*10 - V_x))
 
 def generate_dataset(dynamics_model, num_samples):
 	x = np.array([env.observation_space.sample() for _ in range(num_samples)])
-	u = np.array([env.action_space.sample() for _ in range(num_samples)])
-	fxu = dynamics_model.predict([x,u])
+	# u = np.array([env.action_space.sample() for _ in range(num_samples)])
+	# fxu = dynamics_model.predict([x,u])
 	p = np.repeat([[1,0,0]], num_samples, axis=0) 
 	# (1,0,0) means no movement and pointing upwards
-	return [x, fxu, p]
+	return [x, p]
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--ckpt_path",type=str,default="/home/bmabsout/Documents/gymfc-nf1/training_code/neuroflight_trainer/dynamics_learning/saved/ff44f9/checkpoints/checkpoint50.tf")
-parser.add_argument("--num_samples",type=int,default=100_000)
-parser.add_argument("--epochs",type=int,default=10)
-parser.add_argument("--batch_size", type=int,default=1024)
-parser.add_argument("--epsilon", type=float,default=0.001)
-parser.add_argument("--lr",type=float, default=0.01)
+parser.add_argument("--ckpt_path",type=str,default="./saved/428b23/checkpoints/checkpoint210.tf")
+parser.add_argument("--num_samples",type=int,default=1000_000)
+parser.add_argument("--epochs",type=int,default=5)
+parser.add_argument("--batch_size", type=int,default=2024)
+parser.add_argument("--epsilon", type=float,default=0.01)
+parser.add_argument("--lr",type=float, default=1e-2)
 parser.add_argument("--hyper_diff", type=float, default=1.0)
 parser.add_argument("--hyper_psd", type=float, default=1.0)
 
 args = parser.parse_args()
 
 env = gym.make('Pendulum-v0')
+print(env.action_space.shape)
+action_shape = env.action_space.shape
+state_shape = env.observation_space.shape
 dynamics_model = keras.models.load_model(args.ckpt_path)
 print()
 print()
 print("dynamics_model:")
 dynamics_model.summary()
 
+actor = actor_def(state_shape, action_shape)
+lyapunov_model = V_def(state_shape)
+
+full_model = full_model_def(dynamics_model, actor, lyapunov_model, state_shape)
+full_model.compile(loss=lyapunov_loss, optimizer=keras.optimizers.Adam(lr=args.lr))
+
 X = generate_dataset(dynamics_model,args.num_samples)
-lyapunov_model = compile_lyapunov_model(env.observation_space.shape)
-lyapunov_model.fit(X, epochs=args.epochs, batch_size=args.batch_size)
+full_model.fit(X, epochs=args.epochs, batch_size=args.batch_size)
+
+lyapunov_model.save("lyapunov.tf")
+
+actor.save(args.ckpt_path +".actor.tf")
--- src/test.py
+++ src/test.py
@@ -1,26 +1,33 @@
 import envs.KerasPendulumDir.KerasPendulum
 import gym
 import numpy as np
+from tensorflow import keras
 
 env = gym.make('KerasPendulum-v0',
-	model_path="./saved/256769/checkpoints/checkpoint40.tf")
+	model_path="./saved/428b23/checkpoints/checkpoint210.tf")
 
+saved_actor = keras.models.load_model("./saved/428b23/checkpoints/checkpoint210.tf.actor.tf")
+saved_actor.summary()
 
 orig_env = gym.make('Pendulum-v0')
-# seed = np.random.randint(1000000)
+seed = np.random.randint(1000000)
 # seed = 154911 # almost rotate
-seed = 47039 # almost rotate, then rotate
+# seed = 47039 # almost rotate, then rotate
 # seed = 364366 # rotate
 print("seed:", seed)
 env.seed(seed)
 orig_env.seed(seed)
-env.reset()
-orig_env.reset()
+env_obs = env.reset()
+orig_env_obs = orig_env.reset()
+print("saved_actor")
 
 for i in range(200):
-	random_act = np.random.uniform(2,size=(1,))
-	env_obs, env_reward, env_done, env_info = env.step(random_act)
-	orig_env_obs, orig_env_reward, orig_env_done, orig_env_info = orig_env.step(random_act)
+	# random_act = np.random.uniform(2,size=(1,))
+	act = saved_actor.predict([np.array([env_obs])])[0]
+	print(env_obs)
+	orig_act = saved_actor.predict([np.array([orig_env_obs])])[0]
+	env_obs, env_reward, env_done, env_info = env.step(act)
+	orig_env_obs, orig_env_reward, orig_env_done, orig_env_info = orig_env.step(orig_act)
 	print(np.linalg.norm(env_obs-orig_env_obs))
 	env.render()
 	orig_env.render()
