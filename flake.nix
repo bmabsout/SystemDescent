842a623a9b30348af1c9526c80a0e20e6343bf3f
Bassel Mabsout <bmabsout@hotmail.com>
2021-09-17 10:18:59 -0400

    updated flake to support mach-nix for a more general reproducible python env

--- flake.nix
+++ flake.nix
@@ -4,7 +4,10 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "nixpkgs/ff1ea3a36c1dfafdf0490e0d144c5a0ed0891bb9";
-  outputs = { self, flake-utils, nixpkgs }:
+  inputs.mach-nix = {
+      url = github:DavHau/mach-nix;
+  };
+  outputs = { self, flake-utils, nixpkgs, mach-nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = (import (nixpkgs) { config = {allowUnfree = true;}; system =
               "x86_64-linux";
@@ -12,21 +15,40 @@
           extensions = (with pkgs.vscode-extensions; [
             ms-python.python
             ms-toolsai.jupyter
+            jnoortheen.nix-ide
           ]);
+
+          mach-nix-utils = import mach-nix {
+            inherit pkgs;
+            python = "python39";
+          };
 
           vscodium-with-extensions = pkgs.vscode-with-extensions.override {
             vscode = pkgs.vscodium;
             vscodeExtensions = extensions;
           };
+
+          python-with-deps = mach-nix-utils.mkPython {
+            _.box2d-py = { nativeBuildInputs.add = with pkgs; [ swig ]; }; 
+            providers.pyglet="nixpkgs";
+            requirements= ''
+              numpy
+              tensorflow
+              scipy
+              gym
+              tqdm
+              mypy
+              matplotlib
+              box2d-py
+            '';
+          };
       in {
         devShell = pkgs.mkShell {
-          buildInputs = [
-            (pkgs.python39.withPackages (ps: with ps; 
-              [ numpy tqdm matplotlib scipy gym tensorflow mypy])
-            )
+          buildInputs=[
+            python-with-deps
             vscodium-with-extensions
+            pkgs.swig
           ];
-          # QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.qt5.qtbase.bin}/lib/qt-${pkgs.qt5.qtbase.version}/plugins";
         };
       }
     );
