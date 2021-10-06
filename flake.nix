b4d979caf19c7bd01c2cd289baebd72ea2fba9e5
Bassel Mabsout <bmabsout@hotmail.com>
2021-10-06 15:16:21 -0400

    added pybulletquads

--- flake.nix
+++ flake.nix
@@ -2,13 +2,16 @@
 
   description = "A reproducible environment for learning certifiable controllers";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  # inputs.nixpkgs.url = "nixpkgs/ff1ea3a36c1dfafdf0490e0d144c5a0ed0891bb9";
-  inputs.nixpkgs.url = "nixpkgs/6120ac5cd201f6cb593d1b80e861be0342495be9";
-  inputs.mach-nix = {
-      url = github:DavHau/mach-nix;
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/6120ac5cd201f6cb593d1b80e861be0342495be9";
+    mach-nix.url = github:DavHau/mach-nix;
+    # gym-pybullet-drones.url = "github:utiasDSL/gym-pybullet-drones";
+    gym-pybullet-drones.url = "path:./gym-pybullet-drones";
+    gym-pybullet-drones.flake = false;
   };
-  outputs = { self, flake-utils, nixpkgs, mach-nix }:
+
+  outputs = { self, flake-utils, nixpkgs, mach-nix, gym-pybullet-drones }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = (import (nixpkgs) { config = {allowUnfree = true;}; system =
               "x86_64-linux";
@@ -33,6 +36,7 @@
             _.box2d-py = { nativeBuildInputs.add = with pkgs; [ swig ]; }; 
             providers.pyglet="nixpkgs";
             providers.pygame="nixpkgs";
+            providers.pybullet="nixpkgs";
 
             requirements= ''
               numpy
@@ -44,7 +48,13 @@
               matplotlib
               box2d-py
               pygame
+              pybullet
             '';
+            packagesExtra=[
+              (mach-nix-utils.buildPythonPackage {
+                src = gym-pybullet-drones;
+              })
+            ];
           };
       in {
         devShell = pkgs.mkShell {
