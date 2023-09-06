cc7b36dde636c1db8f8d025ece78679351170e3b
Bassel Mabsout <bmabsout@hotmail.com>
2023-09-06 16:43:08 -0400

    simplified p_mean code which now supports empty arrays and arbitrary axes to calculate a p_mean on, and added linear piecewise functions

--- sd/dfl.py
+++ sd/dfl.py
@@ -2,69 +2,70 @@ import tensorflow as tf
 import numpy as np
 from typing import NamedTuple, Union, Dict, Tuple
 
-#handles batches
-# def geo(l, slack=0.0):
-#     l_arr = np.array(l)
-#     if(len(l_arr.shape) == 1): # batch dimension is 0
-#         l_arr = np.array([l_arr])
-#     slacked = l_arr+slack
-#     res = np.zeros(l_arr.shape[0])
-#     non_zeros = (slacked > 1e-20).all(axis=1)
-#     res[non_zeros] = np.exp(np.mean(np.log(slacked[non_zeros]),axis=1))-slack
-#     return res.squeeze()
 
 @tf.function
-def geo(l, slack=0.0,**kwargs): # doesn't handle batches correctly
-    # n = tf.cast(tf.size(l), tf.float32)
-    # < 1e-30 because nans start appearing out of nowhere otherwise
-    slacked = l+slack
-    mean_log = tf.reduce_mean(tf.math.log(tf.maximum(slacked, 1e-20)))
-    if(tf.reduce_any(slacked < 1e-20)):
-        return 0.0
-    else:
-        return tf.exp(mean_log)-slack
-    # return tf.reduce_prod(tf.where(slacked < 1e-30, 0., slacked)**(1.0/n), **kwargs) - slack
+def tf_pop(tensor, axis):
+    return tf.concat([tf.slice(tensor, [0], [axis]), tf.slice(tensor, [axis+1], [-1])], 0)
 
 
 @tf.function
-def p_mean(l, p, slack=0.0, **kwargs):
+def p_mean(l, p: float, slack=1e-15, default_val=tf.constant(0.0), axis=None):
     """
-    generalized mean, p = -1 is the harmonic mean, p = 1 is the regular mean, p=inf is the max function ...
+    The Generalized mean
+    l: a tensor of elements we would like to compute the p_mean with respect to, elements must be > 0.0
+    p: the value of the generalized mean, p = -1 is the harmonic mean, p = 1 is the regular mean, p=inf is the max function ...
+    slack: allows elements to be at 0.0 with p < 0.0 without collapsing the pmean to 0.0 fully allowing useful gradient information to leak
+    axis: axis or axese to collapse the pmean with respect to, None would collapse all
     https://www.wolframcloud.com/obj/26a59837-536e-4e9e-8ed1-b1f7e6b58377
     """
-    if p == 0.:
-        return geo(tf.abs(l), slack, **kwargs) # there's an issue with this
-    # elif p == np.inf:
-    #     return tf.reduce_max(l)
-    # elif p == -np.inf:
-    #     return tf.reduce_min(l)
-    # else:
-    slacked = tf.abs(l) + slack
-    return tf.reduce_mean(slacked**p, **kwargs)**(1.0/p) - slack
+    p = tf.cast(p, tf.float32)
+    p = tf.where(tf.cast(p, tf.float32) < 1e-5, 1e-5, p)
+
+    return tf.cond(tf.reduce_prod(tf.shape(l)) == 0 # condition if an empty array is fed in
+        , lambda: tf.broadcast_to(default_val, tf_pop(tf.shape(l), axis)) if axis else default_val
+        , lambda: tf.reduce_mean((l + slack)**p, axis=axis))**(1.0/p) - slack
+        # tf.debugging.assert_greater_equal(l, 0.0)
+
 
 @tf.function
-def i_mean(l, p):
+def i_mean(l, p, **kwargs):
     """inverse of p_mean, changes focus point from 0.0 to 1.0 (how far you are from 1 rather than 0)"""
-    return 1.0 - p_mean(1.0 - l, p)
+    return 1.0 - p_mean(1.0 - l, p, **kwargs)
 
+# build_piecewise([(-1.0, 0.0), (-0.1, 0.1), (0.0, 0.2), (line, 0.9), (1.0, 1.0)])
+# tf.where(val < -0.1, transform(val, -1.0, -0.1, 0.0, 0.1), tf.where(val < 0.0, transform(val, -0.1, 0.0, 0.1, 0.2), tf.where(val < line, transform(val, 0.0, line, 0.2, 0.9), transform(val, line, 1.0, 0.9, 1.0))))
 @tf.function
-def transform(x, from_low, from_high, to_low, to_high):
+def transform(x, from_low, from_high, to_low, to_high, clipped=False):
     diff_from = tf.maximum(from_high - from_low, 1e-20)
     diff_to = tf.maximum(to_high - to_low, 1e-20)
-    return (x - from_low)/diff_from * diff_to + to_low
+    mapped = (x - from_low)/diff_from * diff_to + to_low
+    return tf.clip_by_value(mapped, to_low, to_high) if clipped else mapped
+
+@tf.function
+def build_piecewise(xy, val, clipped=False):
+        items = reversed(list(zip(xy, xy[1:])))
+        (prev_x, prev_y), (x, y) = next(items)
+        final = transform(val, prev_x, x, prev_y, y, clipped=clipped)
+        # str = f"transform(val, {prev_x}, {x}, {prev_y}, {y})"
+        for (prev_x, prev_y), (x, y) in items:
+            final = tf.where(val < x, transform(val, prev_x, x, prev_y, y, clipped=clipped), final)
+            # str = f"tf.where(val < {x}, transform(val, {prev_x}, {x}, {prev_y}, {y}), {str})"
+        # print(str)
+        return final
+
 
 @tf.function
 def inv_sigmoid(x):
     return tf.math.log(x/(1-x))
 
 @tf.function
-def smooth_constraint(x, from_low, from_high, to_low=0.03, to_high=0.97, starts_linear=False):
+def smooth_constraint(x, from_low, from_high, to_low=0.03, to_high=0.97, starts_linear=False, clipped=False):
     """like transform but is sigmoidal outside the range instead of linear"""
     scale = lambda x: x*2.0 - 1.0 if starts_linear else x
     unscale = lambda x: (x+1.0)/2.0 if starts_linear else x
     sigmoid_low = inv_sigmoid(unscale(to_low))
     sigmoid_high = inv_sigmoid(unscale(to_high))
-    return scale(tf.sigmoid(transform(x, from_low, from_high, sigmoid_low, sigmoid_high)))
+    return scale(tf.sigmoid(transform(x, from_low, from_high, sigmoid_low, sigmoid_high, clipped=clipped)))
 
 
 def tensor_to_str(tensor: tf.Tensor):
@@ -85,13 +86,13 @@ class Constraints(NamedTuple):
     operator: tf.Tensor
     constraints: Dict[str, 'DFL']
     def scalarize(self):
-        return p_mean(tf.stack(list(map(dfl_scalar, self.constraints.values()))), self.operator)
+        return p_mean(tf.stack(list(map(dfl_scalar, self.constraints.values()))), self.operator, default_val=1.0)
     def __str__(self):
         return f"{self.operator:.2g}<{' '.join(map(format_constraint, self.constraints.items()))}>"
 
 class InvConstraints(Constraints):
     def scalarize(self):
-        return 1.0 - p_mean(tf.stack(list(map(lambda x: 1.0 - dfl_scalar(x), self.constraints.values()))),self.operator)
+        return 1.0 - p_mean(tf.stack(list(map(lambda x: 1.0 - dfl_scalar(x), self.constraints.values()))),self.operator, default_val=1.0)
     def __str__(self):
         return f"{self.operator:.2g}<!{' '.join(map(format_constraint, self.constraints.items()))}!>"
 
