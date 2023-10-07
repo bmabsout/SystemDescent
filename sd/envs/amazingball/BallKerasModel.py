fde2df74f166e16e0323e75b4cbd0b25885f3775
Weifan <chenweifan9@gmail.com>
2023-10-07 00:09:03 -0400

    ballkerasmodel successfully pickled

--- sd/envs/amazingball/BallKerasModel.py
+++ sd/envs/amazingball/BallKerasModel.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 import numpy as np
-import pybullet as p
 import time
 from tensorflow import keras
 from keras import layers
 # import dill
 from sd import utils
 
+# @tf.function
 def ball_differential_eq(states, actions):
     """
     ## Description
@@ -70,29 +70,24 @@ def ball_differential_eq(states, actions):
     pl_rot_x, pl_rot_y,  pl_vel_x, pl_vel_y, ba_pos_x, ba_pos_y, ba_v_x, ba_v_y = tf.split(states, num_or_size_splits=8, axis=1) 
     sp_x, sp_y = tf.split(actions, num_or_size_splits=2, axis=1)
 
-    # clip actions
+    # clip_by_value actions
     sp_x = tf.clip_by_value(sp_x, -max_rot_x, max_rot_x)
     sp_y = tf.clip_by_value(sp_y, -max_rot_y, max_rot_y)
 
     # update plate angular velocity direction, vel is assumed to be constant
     # if the plate rot is within one update of the setpoint, set rot to setpoint, 
     # else, set the plate vel to the corresponding direction 
-    dist = abs(dt * pl_vel)
-    if (dist >= abs(sp_x - pl_rot_x)):
-        pl_rot_x = sp_x
-        pl_vel_x = 0
-    else:
-        pl_vel_x = tf.sign(sp_x - pl_rot_x) * pl_vel
+    dist = tf.abs(dt * pl_vel)
+    pl_rot_x, pl_vel_x = tf.cond(dist >= tf.abs(sp_x - pl_rot_x), 
+                                 lambda: (sp_x, 0.0), 
+                                 lambda: (pl_rot_x, tf.sign(sp_x - pl_rot_x) * pl_vel))
+    pl_rot_y, pl_vel_y = tf.cond(dist >= tf.abs(sp_y - pl_rot_y), 
+                                 lambda: (sp_y, 0.0), 
+                                 lambda: (pl_rot_y, tf.sign(sp_y - pl_rot_y) * pl_vel))
 
-    if (dist >= abs(sp_y - pl_rot_y)):
-        pl_rot_y = sp_y
-        pl_vel_y = 0
-    else:
-        pl_vel_y = tf.sign(sp_y - pl_rot_y) * pl_vel
-
-    # update plate rot
-    new_pl_rot_x = tf.clip(pl_rot_x + pl_vel_x * dt, -max_rot_x, max_rot_x)
-    new_pl_rot_y = tf.clip(pl_rot_y + pl_vel_y * dt, -max_rot_y, max_rot_y)
+    # update plate rot -- step dt wrt angular velocity
+    new_pl_rot_x = tf.clip_by_value(pl_rot_x + pl_vel_x * dt, -max_rot_x, max_rot_x)
+    new_pl_rot_y = tf.clip_by_value(pl_rot_y + pl_vel_y * dt, -max_rot_y, max_rot_y)
 
     # update states
     new_v_x = g * tf.sin(new_pl_rot_x) * dt + ba_v_x
@@ -100,14 +95,28 @@ def ball_differential_eq(states, actions):
     new_pos_x = new_v_x * dt + ba_pos_x  
     new_pos_y = new_v_y * dt + ba_pos_y
 
-    # if new pos is out of bound, set pos to bound and vel to 0
-    if (abs(new_pos_x) >= max_ball_pos_x):
-        new_v_x = 0
-    new_pos_x = tf.clip(new_pos_x, -max_ball_pos_x, max_ball_pos_x)
+    # if new pos is out of bound, set vel to 0
+    new_v_x = tf.cond(tf.abs(new_pos_x) >= max_ball_pos_x, lambda: 0.0, lambda: new_v_x)
+    new_v_y = tf.cond(tf.abs(new_pos_y) >= max_ball_pos_y, lambda: 0.0, lambda: new_v_y)
 
-    if (abs(new_pos_y) >= max_ball_pos_y):
-        new_v_y = 0
-    new_pos_y = tf.clip(new_pos_y, -max_ball_pos_y, max_ball_pos_y)
+    # clip ball position
+    new_pos_x = tf.clip_by_value(new_pos_x, -max_ball_pos_x, max_ball_pos_x)
+    new_pos_y = tf.clip_by_value(new_pos_y, -max_ball_pos_y, max_ball_pos_y)
 
     new_state = tf.concat([new_pl_rot_x, new_pl_rot_y, pl_vel_x, pl_vel_y, new_pos_x, new_pos_y, new_v_x, new_v_y], axis=1)
     return new_state
+
+def amazingball_diff_model():
+    input_state = keras.Input(shape=(8,))
+    input_action = keras.Input(shape=(2,))
+    latent_input = keras.Input(shape=(0,))
+    inputs = layers.Concatenate()([input_state, input_action, latent_input])
+    outputs = layers.Lambda(lambda x: ball_differential_eq(x[:, 0:8], x[:, 8:10]))(inputs)
+    model = keras.Model(inputs={"state": input_state, "action": input_action, "latent": latent_input}, outputs=outputs)
+    model.summary()
+    return model
+
+if __name__ == "__main__":
+    model = amazingball_diff_model()
+    filepath = utils.random_subdir("models/AmazingBall-v0")
+    utils.save_checkpoint(model=model, path=filepath, id=0, extra_objs={"ball_differential_eq": ball_differential_eq})
